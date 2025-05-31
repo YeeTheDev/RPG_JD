d6596939016b5b6cd5cf921bdbdb912f1e553b65
Yee <yee.devindie@gmail.com>
2025-05-31 08:51:15 -0600

    L9 - Creating a Script & understanding variables

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rb;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}

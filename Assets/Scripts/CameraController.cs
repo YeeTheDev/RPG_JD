67ecd185ebc36316d11fa838e92cb0b5a70e54b8
Yee <yee.devindie@gmail.com>
2025-06-07 23:58:50 -0600

    L24 - Keeping Camera inside the map

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,22 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CameraController : MonoBehaviour
 {
     [SerializeField] Transform target;
+    [SerializeField] Tilemap map;
 
-    // Start is called before the first frame update
+    private Vector3 bottomLeftLimit;
+    private Vector3 topRightLimit;
+
+    private void Awake()
+    {
+        bottomLeftLimit = map.localBounds.min;
+        topRightLimit = map.localBounds.max;
+    }
+
     void Start()
     {
         target = PlayerController.instance.transform;
     }
 
-    // Update is called once per frame
     void LateUpdate()
     {
         Vector3 followPosition = target.position;
+        followPosition.x = Mathf.Clamp(followPosition.x, bottomLeftLimit.x, topRightLimit.x);
+        followPosition.y = Mathf.Clamp(followPosition.y, bottomLeftLimit.y, topRightLimit.y);
         followPosition.z = transform.position.z;
+
         transform.position = followPosition;
     }
 }

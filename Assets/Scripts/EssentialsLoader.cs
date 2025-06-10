608f4e5cb684ebaa6afa3b8a472caf95ca3fc553
Yee <yee.devindie@gmail.com>
2025-06-09 20:14:13 -0600

    L42 - Creating a Game Manager

--- Assets/Scripts/EssentialsLoader.cs
+++ Assets/Scripts/EssentialsLoader.cs
@@ -6,10 +6,12 @@ public class EssentialsLoader : MonoBehaviour
 {
     [SerializeField] GameObject uIScreen;
     [SerializeField] GameObject player;
+    [SerializeField] GameObject gameManager;
 
     private void OnEnable()
     {
         if (UIFade.instance == null) { Instantiate(uIScreen); }
-        if(PlayerController.instance == null) { Instantiate(player); }
+        if (PlayerController.instance == null) { Instantiate(player); }
+        if (GameManager.instance == null) { Instantiate(gameManager); }
     }
 }

38bbdc5c49c5b6e0510b4d6e7ba1cda1ef766bef
Yee <yee.devindie@gmail.com>
2025-06-11 21:15:02 -0600

    L56 - Showing the Items in menu

--- Assets/Scripts/GameMenu.cs
+++ Assets/Scripts/GameMenu.cs
@@ -22,6 +22,8 @@ public class GameMenu : MonoBehaviour
         statusArmor, statusArmPower, statusExp;
     public Image statusImage;
 
+    public ItemButton[] itemButtons;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -112,5 +114,25 @@ public class GameMenu : MonoBehaviour
         statusArmPower.text = playerStats[selected].armorPower.ToString();
         statusExp.text = $"{playerStats[selected].currentEXP}/{playerStats[selected].expToNextLevel[playerStats[selected].playerLevel]}";
         statusImage.sprite = playerStats[selected].charImage;
+    }
+
+    public void ShowItems()
+    {
+        for (int i = 0; i < itemButtons.Length; i++)
+        {
+            itemButtons[i].buttonValue = i;
+
+            if (GameManager.instance.itemsHeld[i] != "")
+            {
+                itemButtons[i].buttonImage.gameObject.SetActive(true);
+                itemButtons[i].buttonImage.sprite = GameManager.instance.GetItemDetails(GameManager.instance.itemsHeld[i]).itemSprite;
+                itemButtons[i].amountText.text = GameManager.instance.numberOfItems[i].ToString();
+            }
+            else
+            {
+                itemButtons[i].buttonImage.gameObject.SetActive(false);
+                itemButtons[i].amountText.text = "";
+            }
+        }
     }
 }

938c0121ea86c961c17e1ad02be8dab7830d914e
Yee <yee.devindie@gmail.com>
2025-06-14 12:36:55 -0600

    L72 - Checking & Marking Quests

--- Assets/Scripts/QuestManager.cs
+++ Assets/Scripts/QuestManager.cs
@@ -18,4 +18,39 @@ public class QuestManager : MonoBehaviour
     {
         questMarkersCompleted = new bool[questMarkerNames.Length];
     }
+
+    public int GetQuestNumber(string questToFind)
+    {
+        for (int i = 0; i < questMarkerNames.Length; i++)
+        {
+            if (questMarkerNames[i] == questToFind)
+            {
+                return i;
+            }
+        }
+
+        Debug.LogError($"Quest {questToFind}, does not exist.");
+        return 0;
+    }
+
+    public bool CheckIfComplete(string questToCheck)
+    {
+        int questNumber = GetQuestNumber(questToCheck);
+        if (questNumber != 0)
+        {
+            return questMarkersCompleted[questNumber];
+        }
+
+        return false;
+    }
+
+    public void MarkQuestComplete(string questToMark)
+    {
+        questMarkersCompleted[GetQuestNumber(questToMark)] = true;
+    }
+
+    public void MarkQuestIncomplete(string questToMark)
+    {
+        questMarkersCompleted[GetQuestNumber(questToMark)] = false;
+    }
 }

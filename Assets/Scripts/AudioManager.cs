f16e3ee56090614fdb8494c7285923fdd25375f9
Yee <yee.devindie@gmail.com>
2025-06-14 18:02:27 -0600

    L80  - Adding Audio

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+    public AudioSource[] sfx;
+    public AudioSource[] bgm;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else { Destroy(gameObject); }
+    }
+
+    public void PlaySFX(int soundIndex)
+    {
+        if (soundIndex < sfx.Length) { sfx[soundIndex].Play(); }
+    }
+
+    public void PlayBGM(int musicIndex)
+    {
+        StopMusic();
+
+        if (musicIndex < bgm.Length)
+        {
+            bgm[musicIndex].Play();
+        }
+    }
+
+    public void StopMusic()
+    {
+        for (int i = 0; i < bgm.Length; i++)
+        {
+            bgm[i].Stop();
+        }
+    }
+}

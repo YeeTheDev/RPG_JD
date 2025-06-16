ecf862ea317ccd50dbdd4f3cf6f0d55e030fa36e
Yee <yee.devindie@gmail.com>
2025-06-16 16:58:58 -0600

    L99 - Handling Dead Battlers

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -170,6 +170,16 @@ public class BattleManager : MonoBehaviour
             GameManager.instance.battleActive = false;
             battleActive = false;
         }
+        else
+        {
+            while (activeBattlers[currentTurn].currentHP == 0)
+            {
+                if (currentTurn >= activeBattlers.Count)
+                {
+                    currentTurn = 0;
+                }
+            }
+        }
     }
 
     public IEnumerator EnemyMoveCoroutine()
